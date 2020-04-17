108484feeda5e32b11f4e60b09b57e078a7a3b3f
Ulrik Rasmussen <ulrik.rasmussen@deondigital.com>
2020-04-17 11:23:44 +0200

    reproduce Kryo exception

--- workflows/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
+++ workflows/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
@@ -1,6 +1,8 @@
 package com.template
 
+import com.template.flows.Initiator
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.messaging.startFlow
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.TestIdentity
 import net.corda.testing.driver.DriverDSL
@@ -9,7 +11,6 @@ import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.driver
 import org.junit.Test
 import java.util.concurrent.Future
-import kotlin.test.assertEquals
 
 class DriverBasedTest {
     private val bankA = TestIdentity(CordaX500Name("BankA", "", "GB"))
@@ -20,13 +21,8 @@ class DriverBasedTest {
         // Start a pair of nodes and wait for them both to be ready.
         val (partyAHandle, partyBHandle) = startNodes(bankA, bankB)
 
-        // From each node, make an RPC call to retrieve another node's name from the network map, to verify that the
-        // nodes have started and can communicate.
-
-        // This is a very basic test: in practice tests would be starting flows, and verifying the states in the vault
-        // and other important metrics to ensure that your CorDapp is working as intended.
-        assertEquals(bankB.name, partyAHandle.resolveName(bankB.name))
-        assertEquals(bankA.name, partyBHandle.resolveName(bankA.name))
+        val flow = partyAHandle.rpc.startFlow(::Initiator, partyBHandle.nodeInfo.legalIdentities.single())
+        flow.returnValue.getOrThrow()
     }
 
     // Runs a test inside the Driver DSL, which provides useful functions for starting nodes, etc.
--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -1,20 +1,27 @@
 package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import com.github.andrewoma.dexx.kollection.immutableMapOf
 import net.corda.core.flows.*
+import net.corda.core.identity.Party
 import net.corda.core.utilities.ProgressTracker
+import net.corda.core.utilities.unwrap
 
 // *********
 // * Flows *
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
+class Initiator(private val counterParty: Party) : FlowLogic<Unit>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
     override fun call() {
-        // Initiator flow logic goes here.
+        val session = initiateFlow(counterParty)
+        // Instantiate an immutable map as a stack variable before suspending the flow
+        val map = immutableMapOf("foo" to "bar")
+        val response = session.sendAndReceive<String>("ping")
+        response.unwrap { data -> println("$data, $map") }
     }
 }
 
@@ -22,6 +29,7 @@ class Initiator : FlowLogic<Unit>() {
 class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
-        // Responder flow logic goes here.
+        val request = counterpartySession.receive<String>()
+        counterpartySession.send("pong")
     }
 }
